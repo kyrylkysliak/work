5096577bf391697e06423d4d8228b578102fe91d
Kyryl Kysliak <137636337+kyrylkysliak@users.noreply.github.com>
2023-06-29 15:54:50 +0200

    Create additiona;.py

---
+++ additiona;.py
@@ -0,0 +1,50 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        print("Error: Division by zero!")
+        return None
+
+print("Welcome to the Simple Calculator!")
+
+while True:
+    print("Select operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    choice = input("Enter your choice (1-5): ")
+
+    if choice == '5':
+        print("Thank you for using the Simple Calculator. Goodbye!")
+        break
+
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    if choice == '1':
+        result = add(num1, num2)
+        print(f"{num1} + {num2} = {result}")
+    elif choice == '2':
+        result = subtract(num1, num2)
+        print(f"{num1} - {num2} = {result}")
+    elif choice == '3':
+        result = multiply(num1, num2)
+        print(f"{num1} * {num2} = {result}")
+    elif choice == '4':
+        result = divide(num1, num2)
+        if result is not None:
+            print(f"{num1} / {num2} = {result}")
+    else:
+        print("Invalid choice. Please select a valid option (1-5).")
